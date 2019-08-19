b01be0229658e9309e2aef97ec69d20b150295ba
Sarveshchougule <sarveshchougule@gmail.com>
2019-08-19 15:02:54 +0530

    Assignment DLL is sucessfully completed...

---
+++ 08_DLL/03_DLLClient/04_Def_Explicit/Hello/Hello.cpp
@@ -0,0 +1,80 @@
+#include<windows.h>
+
+#include "MyMath.h"
+
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow) {
+
+	WNDCLASSEX wndclass;
+	HWND hwnd;
+	MSG msg;
+	TCHAR szAppName[] = TEXT("My App");
+
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.hInstance = hInstance;
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.lpszClassName = szAppName;
+	wndclass.lpszMenuName = NULL;
+
+
+	RegisterClassEx(&wndclass);
+
+	hwnd = CreateWindow(szAppName,
+		TEXT("SKC APPLICATION"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	ShowWindow(hwnd, iCmdShow);
+	UpdateWindow(hwnd);
+
+	while (GetMessage(&msg, NULL, 0, 0)) {
+
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return((int)msg.wParam);
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam) {
+
+	int i, j;
+	i = 25;
+	HINSTANCE hDll;
+	typedef int(*MakeSquarefn) (int);
+	MakeSquarefn pfn;
+	TCHAR str[255];
+	switch (iMsg) {
+		
+	case WM_CREATE:
+		hDll = LoadLibrary(TEXT("MyMath.dll"));
+		pfn = (MakeSquarefn)GetProcAddress(hDll, "MakeSquare");
+		j = pfn(i);
+		wsprintf(str, TEXT("Square of %d is %d"), i, j);
+		MessageBox(hwnd, str, TEXT("Message"), MB_OK | MB_ICONEXCLAMATION);
+		FreeLibrary(hDll);
+		break;
+		
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	}
+	return (DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
+
+
